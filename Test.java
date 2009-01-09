4cfae0317ee9ded524ecd34e3048c36b64ec02ab
FH <fh3095-github@yahoo.de>
2009-01-09 23:37:43 +0100

    Alten Code aus der Test-Main entfernt

--- Test.java
+++ Test.java
@@ -5,7 +5,9 @@ import ngmud.ngMUDException;
 public class Test {
 	public static void main(String[] args) throws ngMUDException { // Test-main-Func
 		CLog.Init("",true);
-		CLog.Error("Cant init CLog");
+		CLog.Info("Log initialized successfull :D");
+		
+		CLog.UnInit();
 	}
 
 }
