7232a46e5081800193e5fe8cdaf1f6c1b0f3496f
FH <fh3095-github@yahoo.de>
2009-02-09 15:04:47 +0100

    Log umgeschrieben auf enum-LogLevels und HTML-Log-Ausgabe erstellt. Die HTML-Ausgabe is uebrigens "This document was successfully checked as HTML 4.01 Transitional!" laut http://validator.w3.org/check :D

--- Test.java
+++ Test.java
@@ -4,8 +4,16 @@ import ngmud.ngMUDException;
 
 public class Test {
 	public static void main(String[] args) throws ngMUDException { // Test-main-Func
-		CLog.Init("",true,(short)5);
+		CLog.Init("",true,CLog.LOG_LEVEL.CUSTOM,(short)5);
 		CLog.Info("Log initialized successfull :D");
+		CLog.Custom("Custom-Test Level 1", 1);
+		CLog.Custom("Custom-Text Level 6", 6);
+		CLog.Custom("Custom-Test Level 5", 5);
+		CLog.Debug("Debug-Test");
+		CLog.Warning("Warnung-Test");
+		CLog.Error("Error-Test");
+		CLog.Force("Force-Test");
+		CLog.CustomForce("Custom-Force Testüüü");
 		
 		CLog.UnInit();
 	}
--- ngmud/CLog.java
+++ ngmud/CLog.java
@@ -4,116 +4,201 @@ import java.io.*;
 import ngmud.ngMUDException;
 
 public class CLog {
-	static protected boolean Inited=false;
-	static protected DataOutputStream Out=null;
-	static protected String Filename="";
-	static protected short LogLevel=0;
+	public enum LOG_LEVEL {
+		CUSTOM_FORCE(-2,"Custom Force"),
+		FORCE(-1,"Force"),
+		ONLY_CUSTOM(0,"Custom"),
+		ERROR (1,"<b><span style=\"color: #FF0000\">Error</span></b>"),
+		WARNING (2,"<b><span style=\"color: #CCCC00\">Warnung</span></b>"),
+		INFO (3,"<b><span style=\"color: #00BB00\">Info</span></b>"),
+		DEBUG (4,"Debug"),
+		CUSTOM (5,"<b>Custom</b>");
+		
+		
+		private final int Num;
+		private final String HtmlStr;
+		LOG_LEVEL(int Num,String HtmlStr)
+		{
+			this.Num=Num;
+			this.HtmlStr=HtmlStr;
+		}
+		
+		public int GetNum()
+		{
+			return Num;
+		}
+		
+		public String GetHtmlText()
+		{
+			return HtmlStr;
+		}
+	}
 	
+	static protected boolean Inited=false;
+	static protected DataOutputStream TxtOut=null;
+	static protected DataOutputStream HtmlOut=null;
+	static protected String Filename="";
+	static protected LOG_LEVEL LogLevel;
+	static protected int CustomLogLevel;
+
 	static public boolean IsInited()
 	{
 		return Inited;
 	}
 	
-	static public void Init(String Filename,boolean Overwrite,short LogLevel) throws ngMUDException
+	static public void Init(String Filename,boolean Overwrite,LOG_LEVEL LogLevel,int CustomLogLevel) throws ngMUDException
 	{
-		if(LogLevel<0)
+		if(LogLevel.GetNum()<0)
 		{
-			throw new ngMUDException("CRITICAL: CLog: LogLevel<0 is not allowed");
+			throw new ngMUDException("CRITICAL: Log-Levels less than zero are not acceptable Log-Levels");
 		}
 		if(Filename=="")
 		{
-			Filename="Log.txt";
+			Filename="Log";
 		}
 		try {
-			Out=new DataOutputStream(new FileOutputStream(Filename,!Overwrite));
+			TxtOut=new DataOutputStream(new FileOutputStream(Filename+".txt",!Overwrite));
+			HtmlOut=new DataOutputStream(new FileOutputStream(Filename+".html",false));
 		}
 		catch(IOException e)
 		{
 			throw new ngMUDException("CRITICAL: CLog: Can't initialize Log-File-Stream.",e);
 		}
+		try {
+			HtmlOut.writeBytes("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\n"+
+			                   "\t\"http://www.w3.org/TR/html4/loose.dtd\">\n"+
+			                   "<html>\n<head>\n"+
+			                   "\t<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\">\n"+
+			                   "\t<title>LogFile</title>\n"+
+			                   "</head>\n\n"+
+			                   "<body>\n<center><h1>LogFile</h1></center><br>\n"+
+			                   "<table border=\"3\">");
+			HtmlOut.writeBytes("<tr><th>Type</th><th>Level</th><th>Method</th><th>Message</th></tr>");
+		}
+		catch(IOException e) { }
 		Inited=true;
 		CLog.Filename=Filename;
 		CLog.LogLevel=LogLevel;
+		CLog.CustomLogLevel=CustomLogLevel;
 	}
 	
 	static protected String GetClassMethod()
 	{
 		StackTraceElement[] STE=Thread.currentThread().getStackTrace();
-		return STE[3].getClassName()+"::"+STE[3].getMethodName()+": ";
+		return STE[3].getClassName()+"::"+STE[3].getMethodName();
 	}
 	
 	static public void Error(String Text)
 	{
-		if(1<=CLog.LogLevel)
+		if(LOG_LEVEL.ERROR.GetNum()<=CLog.LogLevel.GetNum())
 		{
-			Out("ERROR: "+GetClassMethod()+Text);
+			String ClassMethod=GetClassMethod();
+			OutHtml(LOG_LEVEL.ERROR,LOG_LEVEL.ERROR.GetNum(),ClassMethod,Text);
+			OutTxt("ERROR: "+ClassMethod+Text);
 		}
 	}
 	
 	static public void Warning(String Text)
 	{
-		if(2<=CLog.LogLevel)
+		if(LOG_LEVEL.WARNING.GetNum()<=CLog.LogLevel.GetNum())
 		{
-			Out("WARNING: "+GetClassMethod()+Text);
+			String ClassMethod=GetClassMethod();
+			OutHtml(LOG_LEVEL.WARNING,LOG_LEVEL.WARNING.GetNum(),ClassMethod,Text);
+			OutTxt("WARNING: "+ClassMethod+": "+Text);
 		}
 	}
 	
 	static public void Info(String Text)
 	{
-		if(3<=CLog.LogLevel)
+		if(LOG_LEVEL.INFO.GetNum()<=CLog.LogLevel.GetNum())
 		{
-			Out("INFO: "+GetClassMethod()+Text);
+			String ClassMethod=GetClassMethod();
+			OutHtml(LOG_LEVEL.INFO,LOG_LEVEL.INFO.GetNum(),ClassMethod,Text);
+			OutTxt("INFO: "+ClassMethod+": "+Text);
 		}
 	}
 	
 	static public void Debug(String Text)
 	{
-		if(4<=CLog.LogLevel)
+		if(LOG_LEVEL.DEBUG.GetNum()<=CLog.LogLevel.GetNum())
 		{
-			Out("DEBUG: "+GetClassMethod()+Text);
+			String ClassMethod=GetClassMethod();
+			OutHtml(LOG_LEVEL.DEBUG,LOG_LEVEL.DEBUG.GetNum(),ClassMethod,Text);
+			OutTxt("DEBUG: "+ClassMethod+": "+Text);
+		}
+	}
+	
+	static public void CustomForce(String Text)
+	{
+		if(LOG_LEVEL.CUSTOM.GetNum()<=CLog.LogLevel.GetNum() ||
+		   LOG_LEVEL.ONLY_CUSTOM.GetNum()==CLog.LogLevel.GetNum())
+		{
+			String ClassMethod=GetClassMethod();
+			OutTxt("CUSTOM: "+ClassMethod+": "+Text);
+			OutHtml(LOG_LEVEL.CUSTOM_FORCE,-1,ClassMethod,Text);
+		}
+	}
+	
+	static public void Custom(String Text,int Level)
+	{
+		if((LOG_LEVEL.CUSTOM.GetNum()<=CLog.LogLevel.GetNum() ||
+		    LOG_LEVEL.ONLY_CUSTOM.GetNum()<=CLog.LogLevel.GetNum()) &&
+		   Level<=CLog.CustomLogLevel)
+		{
+			String ClassMethod=GetClassMethod();
+			OutHtml(LOG_LEVEL.CUSTOM,Level,ClassMethod,Text);
+			OutTxt("CUSTOM: "+ClassMethod+": "+Text);
 		}
 	}
 	
 	static public void Force(String Text)
 	{
-		Out(GetClassMethod()+Text);
+		String ClassMethod=GetClassMethod();
+		OutHtml(LOG_LEVEL.FORCE,-1,ClassMethod,Text);
+		OutTxt(ClassMethod+": "+Text);
 	}
 	
-	static public void Force(String Text,short LogLevel)
+	static public void OutTxt(String Text)
 	{
-		if(LogLevel<=CLog.LogLevel)
-		{
-			Out(GetClassMethod()+Text);
-		}
-	}
-	
-	static public void Out(String Text)
-	{
-		System.out.println(Text+"\r\n");
+		System.out.println(Text);
+		System.out.flush();
 		try {
-			Out.writeBytes(Text+"\r\n");
-			Out.flush();
+			TxtOut.writeBytes(Text+"\r\n");
+			TxtOut.flush();
 		}
 		catch(IOException e) {}
 	}
 	
-	static public void Out(String Text,short LogLevel)
+	static public void OutHtml(LOG_LEVEL Type,int Level,String Method,String Text)
 	{
-		if(LogLevel<=CLog.LogLevel)
-		{
-			Out(Text);
+		try {
+			HtmlOut.writeBytes("<tr><td>"+Type.GetHtmlText()+"</td><td>"+Level+"</td><td>"+
+			                   Method+"</td><td>"+Text+"</td></tr>\n");
+			HtmlOut.flush();
 		}
+		catch(IOException e) {}
 	}
 	
 	static public void UnInit()
 	{
-		if(Out!=null)
+		if(TxtOut!=null)
 		{
 			try {
-				Out.close();
+				TxtOut.close();
 			}
 			catch(IOException e) {}
-			Out=null;
+			TxtOut=null;
+		}
+		if(HtmlOut!=null)
+		{
+			try {
+				HtmlOut.writeBytes("\n</table><br><br><br>\n\n\n"+
+				                   "<small>&copy; <a href=\"http://ngMUD.ath.cx\">ngMUD</a>"+
+				                   "</small><br>\n</body>\n</html>\n");
+				HtmlOut.close();
+			}
+			catch(IOException e) {}
+			HtmlOut=null;
 		}
 	}
 }
